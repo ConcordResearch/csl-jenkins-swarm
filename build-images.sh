90f821c2fee0b6f2151cdd9d3b1860d50f10f99c
Stephen Rufle <srufle@gmail.com>
2019-01-08 09:47:18 -0700

    Added build-images.sh

---
+++ build-images.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+cd jenkins-swarm-docker-master
+
+echo "Copying ssh keys for master"
+mkdir -p master-ssh
+cp -a ../ssh/ ./master-ssh/
+
+export JENKINS_VERSION="2.157"
+echo "Building Jenkins master"
+docker build -f Dockerfile \
+    --build-arg JENKINS_VERSION=${JENKINS_VERSION} \
+    -t csc/jenkins:${JENKINS_VERSION} \
+    -t csc/jenkins:latest \
+    .
+
+cd ../jenkins-swarm-docker-slave
+
+echo "Copying ssh keys for nodes"
+mkdir -p node-ssh
+cp -a ../ssh/ ./node-ssh/
+
+export DOCKER_VERSION="18.06"
+echo "Building Jenkins node"
+docker build -f Dockerfile \
+    --build-arg DOCKER_VERSION=${DOCKER_VERSION} \
+    -t csc/jenkins-node:${JENKINS_VERSION} \
+    -t csc/jenkins-node:latest \
+    .
